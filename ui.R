08733c9d8060792699c1bda354c3cac99d702d7e
abhishekd1977 <abhishekd.iitb@gmail.com>
2018-01-13 23:39:57 +0530

    Data Tab

--- ui.R
+++ ui.R
@@ -14,33 +14,19 @@ shinyUI(
       # theme = "cerulean",  # <--- To use a theme, uncomment this
       "ML As a Service",
       tabPanel("Classification",
-               sidebarPanel(
-                 fileInput("file", "File input:"),
-                 textInput("txt", "Text input:", "general"),
-                 sliderInput("slider", "Slider input:", 1, 100, 30),
-                 tags$h5("Deafult actionButton:"),
-                 actionButton("action", "Search"),
-                 
-                 tags$h5("actionButton with CSS class:"),
-                 actionButton("action2", "Action button", class = "btn-primary")
-               ),
                mainPanel(
                  tabsetPanel(
-                   tabPanel("CART",
+                   tabPanel("Data",
                             h4("Table"),
-                            tableOutput("table"),
-                            h4("Verbatim text output"),
-                            verbatimTextOutput("txtout"),
-                            h1("Header 1"),
-                            h2("Header 2"),
-                            h3("Header 3"),
-                            h4("Header 4"),
-                            h5("Header 5")
+                            tableOutput("table")
                    ),
+                   tabPanel("Summary", "This panel is intentionally left blank"),
                    tabPanel("Logistic", "This panel is intentionally left blank"),
                    tabPanel("Naive Bayes", "This panel is intentionally left blank"),
                    tabPanel("Neural Networks", "This panel is intentionally left blank"),
-                   tabPanel("Support Vector Machines", "This panel is intentionally left blank")
+                   tabPanel("Support Vector Machines", "This panel is intentionally left blank"),
+                   tabPanel("Model Comparison", "This panel is intentionally left blank"),
+                   tabPanel("Prediction", "This panel is intentionally left blank")
                  )
                )
       ),
