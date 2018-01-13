767cb65a42375b11e377ad476e7353e00de9dde4
abhishekd1977 <abhishekd.iitb@gmail.com>
2018-01-13 21:00:06 +0530

    Initial Version

---
+++ server.R
@@ -0,0 +1,19 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyServer(function(input, output) {
+
+  output$txtout <- renderText({
+    paste(input$txt, input$slider, format(input$date), sep = ", ")
+  })
+  output$table <- renderTable({
+    head(cars, 4)
+  })
+}
+)
