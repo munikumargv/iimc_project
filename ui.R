1c3f956a3052acf0d8ce3d6b8854607530dd1643
abhishekd1977 <abhishekd.iitb@gmail.com>
2018-01-14 15:23:41 +0530

    Data Tab

--- ui.R
+++ ui.R
@@ -12,19 +12,52 @@ shinyUI(
     shinythemes::themeSelector(),
     navbarPage(
       # theme = "cerulean",  # <--- To use a theme, uncomment this
-      "ML As a Service",
+      "Prediction Machine",
       tabPanel("Classification",
                mainPanel(
                  tabsetPanel(
-                   tabPanel("Data",
-                            h4("Table"),
-                            tableOutput("table")
+                   tabPanel("Data Selector",
+                            # Input: Select a file ----
+                            fileInput("file1", "Choose CSV File",
+                                      multiple = TRUE,
+                                      accept = c("text/csv",
+                                                 "text/comma-separated-values,text/plain",
+                                                 ".csv")),
+                            
+                            # Horizontal line ----
+                            tags$hr(),
+                            
+                            # Input: Checkbox if file has header ----
+                            checkboxInput("header", "Header", TRUE),
+                            
+                            # Input: Select separator ----
+                            radioButtons("sep", "Separator",
+                                         choices = c(Comma = ",",
+                                                     Semicolon = ";",
+                                                     Tab = "\t"),
+                                         selected = ","),
+                            
+                            # Input: Select quotes ----
+                            radioButtons("quote", "Quote",
+                                         choices = c(None = "",
+                                                     "Double Quote" = '"',
+                                                     "Single Quote" = "'"),
+                                         selected = '"'),
+                            
+                            # Horizontal line ----
+                            tags$hr(),
+                            
+                            # Input: Select number of rows to display ----
+                            radioButtons("disp", "Display",
+                                         choices = c(Head = "head",
+                                                     All = "all"),
+                                         selected = "head")
                    ),
-                   tabPanel("Summary", "This panel is intentionally left blank"),
-                   tabPanel("Logistic", "This panel is intentionally left blank"),
-                   tabPanel("Naive Bayes", "This panel is intentionally left blank"),
+                   tabPanel("Data Summary", "This panel is intentionally left blank"),
+                   tabPanel("Logistic Regression", "This panel is intentionally left blank"),
+                   tabPanel("Naive Bayes Classifier", "This panel is intentionally left blank"),
                    tabPanel("Neural Networks", "This panel is intentionally left blank"),
-                   tabPanel("Support Vector Machines", "This panel is intentionally left blank"),
+                   tabPanel("SVM", "This panel is intentionally left blank"),
                    tabPanel("Model Comparison", "This panel is intentionally left blank"),
                    tabPanel("Prediction", "This panel is intentionally left blank")
                  )
