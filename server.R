557b75a2d177ab0512d81bf1c30e1a9f9e6d8868
Abhishek Dubey <abhishekdubey@Abhisheks-MacBook-Pro.local>
2018-01-16 21:52:38 +0530

    Data Tab Changes

--- server.R
+++ server.R
@@ -10,16 +10,28 @@ library(DT)
 
 shinyServer(function(input, output) {
   
+  output$ui <- renderUI({"Apollo 13"})
+  
   output$contents <- DT::renderDataTable({
-    req(input$file1)
-    df <- read.csv(input$file1$datapath, sep = input$sep)
-    DT::datatable(df)
+    if(input$radio == 2){
+      req(input$file1)
+      df <- read.csv(input$file1$datapath, sep = input$sep)
+      DT::datatable(df)
+    }
+    else{
+      DT::datatable(mtcars)
+    }
   })
   
   output$summary <- renderPrint({
+    if(input$radio == 2){
     req(input$file1)
     df <- read.csv(input$file1$datapath, sep = input$sep)
     summary(df)
+    }
+    else{
+      summary(mtcars)
+    }
   })
 }
 )
