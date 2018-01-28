fdeb08bc4afeda62878a4fd4893181e9d144a834
abhishekd1977 <abhishekd.iitb@gmail.com>
2018-01-28 19:23:17 +0530

    model

--- global.R
+++ global.R
@@ -10,6 +10,24 @@ selectData <- function (input){
     }
 }
 
+#Split data into training and test datasets
+splitData <- function(inputData, outcome){
+    train.idx <- createDataPartition(inputData[, outcome], p=0.70, list = FALSE)
+    train <- inputData[train.idx,]
+    test <- inputData[-train.idx, ]
+    list(train, test)
+}
+
+#Derive Training Data
+getTrainingData <- function(input){
+    splitData(selectData(input), input$in2)[[1]]
+}
+
+#Derive Test Data
+getTestData <- function(input){
+    splitData(selectData(input), input$in2)[[2]]
+}
+
 #Build Formula
 modelFormula <- function(input){
     y <- input$in2
@@ -19,18 +37,17 @@ modelFormula <- function(input){
 
 #Logistic Regression Model
 logitFunc <- function(input){
-    my.data <- selectData(input)
-    glm(modelFormula(input), data = my.data, family = "binomial")
+    glm(modelFormula(input), data = getTrainingData(input), family = "binomial")
 }
 
 #Naive Bayes Model
 naiveBayesFunc <- function(input){
     my.data <- selectData(input)
-    naiveBayes(modelFormula(input), data = my.data)
+    naiveBayes(modelFormula(input), data = getTrainingData(input))
 }
 
 #Neural Networks Model
 nnetFunc <- function(input){
     my.data <- selectData(input)
-    nnet(modelFormula(input), data = my.data, size=10, decay = 0.025, maxit = 10000)
+    nnet(modelFormula(input), data = getTrainingData(input), size=10, decay = 0.025, maxit = 10000)
 }
