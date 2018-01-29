c2818d33f583e7d5adc44ea77fc43bb3ecd93535
Abhishek Dubey <abhishekdubey@Abhisheks-MacBook-Pro.local>
2018-01-29 18:47:35 +0530

    Data Tab Changes

--- global.R
+++ global.R
@@ -11,21 +11,29 @@ selectData <- function (input){
 }
 
 #Split data into training and test datasets
-splitData <- function(inputData, outcome){
-    train.idx <- createDataPartition(inputData[, outcome], p=0.70, list = FALSE)
-    train <- inputData[train.idx,]
-    test <- inputData[-train.idx, ]
+splitData <- function(inputData, input){
+    #train.idx <- createDataPartition(inputData[, outcome], p=0.70, list = FALSE)
+    #train <- inputData[train.idx,]
+    #test <- inputData[-train.idx, ]
+    
+    ### Make variables factors into factors
+    factor_variables <- input$in2
+    inputData[factor_variables] <- lapply(inputData[factor_variables], function(x) as.factor(x))
+  
+    sample <- sample.split(inputData[, input$in2], SplitRatio = .75)
+    train = subset(inputData, sample == TRUE)
+    test = subset(inputData, sample == FALSE)
     list(train, test)
 }
 
 #Derive Training Data
 getTrainingData <- function(input){
-    splitData(selectData(input), input$in2)[[1]]
+    splitData(selectData(input), input)[[1]]
 }
 
 #Derive Test Data
 getTestData <- function(input){
-    splitData(selectData(input), input$in2)[[2]]
+    splitData(selectData(input), input)[[2]]
 }
 
 #Build Formula
@@ -42,14 +50,17 @@ logitFunc <- function(input){
 
 #Naive Bayes Model
 naiveBayesFunc <- function(input){
-    my.data <- selectData(input)
     naiveBayes(modelFormula(input), data = getTrainingData(input))
 }
 
 #Neural Networks Model
 nnetFunc <- function(input){
-    my.data <- selectData(input)
     nnet(modelFormula(input), data = getTrainingData(input), size=10, decay = 0.025, maxit = 10000)
+}
+
+#Support Vector Machines Model
+svmFunc <- function(input){
+  svm(modelFormula(input), data = getTrainingData(input))
 }
 
 #Confusion Matrix for Naive Bayes
@@ -57,4 +68,28 @@ predict.naivebayes.fulldata <- function(input, outcome){
     test.data <- getTestData(input)
     naivebayes.predicted.fulldata <- predict(naiveBayesFunc(input), test.data)
     table(naivebayes.predicted.fulldata, test.data[,outcome], dnn = c("Predicted", "Actual"))
+}
+
+#Confusion Matrix for Logistic
+predict.logit.fulldata <- function(input, outcome){
+  test.data <- getTestData(input)
+  logit.predicted.fulldata <- predict(logitFunc(input), test.data)
+  logit.predicted.fulldata <- ifelse(logit.predicted.fulldata < 0.7, 0, 1)
+  table(logit.predicted.fulldata, test.data[,outcome], dnn = c("Predicted", "Actual"))
+}
+
+#Confusion Matrix for Neural Networks
+predict.nnet.fulldata <- function(input, outcome){
+  test.data <- getTestData(input)
+  nnet.predicted.fulldata <- predict(nnetFunc(input), test.data)
+  nnet.predicted.fulldata <- ifelse(nnet.predicted.fulldata < 0.7, 0, 1)
+  table(nnet.predicted.fulldata, test.data[,outcome], dnn = c("Predicted", "Actual"))
+}
+
+#Confusion Matrix for SVM
+predict.svm.fulldata <- function(input, outcome){
+  test.data <- getTestData(input)
+  svm.predicted.fulldata <- predict(svmFunc(input), test.data)
+  #svm.predicted.fulldata <- ifelse(svm.predicted.fulldata < 0.7, 0, 1)
+  table(svm.predicted.fulldata, test.data[,outcome], dnn = c("Predicted", "Actual"))
 }
