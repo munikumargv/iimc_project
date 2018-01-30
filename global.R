b3c7d3bdefe6031759184980d47e170f1b2c38a3
Abhishek Dubey <abhishekdubey@Abhisheks-MacBook-Pro.local>
2018-01-30 20:50:37 +0530

    Data Tab Changes

--- global.R
+++ global.R
@@ -1,41 +1,65 @@
 
-#Data Selector
+##--------------------------------Data Processing Starts------------------------
+#Step 1: Data Selector
 selectData <- function (input){
     if(input$radio == 2){
         req(input$file1)
-        df <- read.csv(input$file1$datapath, sep = ",")
+        df <- read.csv(input$file1$datapath, sep = ",", stringsAsFactors = FALSE)
         df
     }else{
         PimaIndiansDiabetes
     }
 }
 
-#Split data into training and test datasets
-splitData <- function(inputData, input){
-    #train.idx <- createDataPartition(inputData[, outcome], p=0.70, list = FALSE)
-    #train <- inputData[train.idx,]
-    #test <- inputData[-train.idx, ]
-    
-    ### Make variables factors into factors
-    factor_variables <- input$in2
-    inputData[factor_variables] <- lapply(inputData[factor_variables], function(x) as.factor(x))
+
+#Step 2: Data Pre-Processing [Convert into factors]
+convertToFactors <- function(inputData, input){
+  ### Convert outcome variable to a factor
+  factor_variables <- input$in2
+  inputData[factor_variables] <- lapply(inputData[factor_variables], function(x) as.factor(x))
   
-    sample <- sample.split(inputData[, input$in2], SplitRatio = .75)
+  #Convert all data frame character columns to factors. [Is this right thing to do?]
+  inputData[sapply(inputData, is.character)] <- lapply(inputData[sapply(inputData, is.character)], as.factor)  
+  inputData
+}
+
+
+#Step 3: Data Pre-Processing [Impute missing data]
+imputeMissingData <- function(inputData, input){
+  ## To be done
+}
+
+#Step 4: Data Pre-Processing [Top Level Function]
+preProcessData <- function(inputData, input){
+  convertToFactors(inputData, input)
+  #imputeMissingData(inputData, input)
+}
+
+#Step 5: Split data into training and test datasets
+splitData <- function(inputData, input){
+    sample <- sample.split(inputData[, input$in2], SplitRatio = 0.75)
     train = subset(inputData, sample == TRUE)
     test = subset(inputData, sample == FALSE)
     list(train, test)
 }
 
-#Derive Training Data
+#Step 6: Derive Training Data
 getTrainingData <- function(input){
-    splitData(selectData(input), input)[[1]]
+  preProcessedData <- preProcessData(selectData(input), input)
+  #imputedData <- imputeMissingData(preProcessedData, input)
+  splitData(preProcessedData, input)[[1]]
 }
 
-#Derive Test Data
+#Step 7: Derive Test Data
 getTestData <- function(input){
-    splitData(selectData(input), input)[[2]]
+  preProcessedData <- preProcessData(selectData(input), input)
+  #imputedData <- imputeMissingData(preProcessedData, input)
+  splitData(preProcessedData, input)[[2]]
 }
 
+##--------------------------------Data Processing Ends------------------------
+
+##--------------------------------Model Building Starts-----------------------
 #Build Formula
 modelFormula <- function(input){
     y <- input$in2
@@ -93,3 +117,4 @@ predict.svm.fulldata <- function(input, outcome){
   #svm.predicted.fulldata <- ifelse(svm.predicted.fulldata < 0.7, 0, 1)
   table(svm.predicted.fulldata, test.data[,outcome], dnn = c("Predicted", "Actual"))
 }
+##--------------------------------Model Building Ends-------------------------
